a7344ff2b604bc34b602d4de5d7732eed3585e02
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-05-17 15:23:49 -0600

    Seção 1, Aula 12 - Sistema Grid

--- social/src/App.vue
+++ social/src/App.vue
@@ -1,36 +1,46 @@
 <template>
-  <span>
-    <header>
-      <navbar cor='green darken-1' logo='Social' url='/' />
-    </header>
+    <span>
+        <header>
+            <navbar cor='green darken-1' logo='Social' url='/' />
+        </header>
 
-    <main>
-      <div class='container'>
-        <router-view/>
-      </div>
-    </main>
+        <main>
+            <div class='container'>
+            <div class='row'>
+                <grid-vue tamanho="4">
+                Menu
+                </grid-vue>
 
-    <footer-vue cor='green darken-1' logo='Social' descricao='Teste de Descrição' >
-      <li><a class="grey-text text-lighten-3" href="#!">Home</a></li>
-      <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-      <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-      <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-    </footer-vue>
-  </span>
+                <grid-vue tamanho="8">
+                <router-view/>
+                </grid-vue>
+            </div>
+            </div>
+        </main>
+
+        <footer-vue cor='green darken-1' logo='Social' descricao='Teste de Descrição' >
+            <li><a class="grey-text text-lighten-3" href="#!">Home</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+        </footer-vue>
+    </span>
 </template>
 
 <script>
 import Navbar from '@/components/layouts/Navbar'
 import FooterVue from '@/components/layouts/FooterVue'
+import GridVue from '@/components/layouts/GridVue'
 
 export default {
-  name: 'App',
-  components: {
-    Navbar,
-    FooterVue
-  },
+    name: 'App',
+    components: {
+        Navbar,
+        FooterVue,
+        GridVue
+    },
 }
-</script>
 
 <style>
+
 </style>
