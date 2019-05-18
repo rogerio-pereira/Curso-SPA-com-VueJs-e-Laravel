159f3eac732e078761c9776551d6d2ad9c1c770b
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-05-18 17:24:11 -0600

    Seção 1, Aula 18 - Componente Publicar Conteudo

---
+++ social/src/components/social/PublicarConteudoVue.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="row">
+        <grid-vue class="input-field" tamanho='12'>
+            <textarea v-model='conteudo' class="materialize-textarea"></textarea>
+            <label>O que está acontecendo?</label>
+        </grid-vue>
+        
+        <p>
+            <grid-vue v-if='conteudo' class='btn waves-effect waves-light' tamanho='2 offset-s10'>Publicar</grid-vue>
+        </p>
+    </div>
+</template>
+
+<script>
+import GridVue from '@/components/layouts/GridVue'
+
+export default {
+    name: 'PublicarConteudoVue',
+    components: {
+        GridVue
+    },
+    props:[
+    ],
+    data () {
+        return {
+            conteudo: ''
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- social/src/pages/home/Home.vue
+++ social/src/pages/home/Home.vue
@@ -1,15 +1,6 @@
 <template>
     <span>
-        <div class="row">
-            <grid-vue class="input-field" tamanho='12'>
-                <textarea id="textarea1" class="materialize-textarea"></textarea>
-                <label for="textarea1">O que está acontecendo?</label>
-            </grid-vue>
-            
-            <p>
-                <grid-vue class='btn waves-effect waves-light' tamanho='2 offset-s10'>Publicar</grid-vue>
-            </p>
-        </div>
+        <publicar-conteudo-vue />
 
         <card-conteudo-vue perfil='https://materializecss.com/images/yuna.jpg' nome='Rogerio Pereira' data='17/05/2019 12:30'>
             <card-detalhe-vue 
@@ -24,14 +15,14 @@
 <script>
 import CardConteudoVue from '@/components/social/CardConteudoVue'
 import CardDetalheVue from '@/components/social/CardDetalheVue'
-import GridVue from '@/components/layouts/GridVue'
+import PublicarConteudoVue from '@/components/social/PublicarConteudoVue'
 
 export default {
     name: 'Home',
     components: {
         CardConteudoVue,
         CardDetalheVue,
-        GridVue
+        PublicarConteudoVue
     },
     data () {
         return {
