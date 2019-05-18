0ad20af9951ef92ecd3b290368c37b253dd3f682
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-05-18 17:14:32 -0600

    Seção 1, Aula 17 - Campo para publicar Conteudo

--- social/src/components/social/CardConteudoVue.vue
+++ social/src/components/social/CardConteudoVue.vue
@@ -1,26 +1,28 @@
 <template>
-    <div class="card">
-        <div class="card-content">
-            <div class="row valign-wrapper">
-                <grid-vue tamanho="1">
-                    <img :src="perfil" :alt="nome" class="circle responsive-img"> <!-- notice the "circle" class -->
-                </grid-vue>
-                <grid-vue tamanho="11">
-                    <span>
-                        <strong>{{nome}}</strong> - <small>{{data}}</small>
-                    </span>
-                </grid-vue>
-            </div>
+    <div class='row'>
+        <div class="card">
+            <div class="card-content">
+                <div class="row valign-wrapper">
+                    <grid-vue tamanho="1">
+                        <img :src="perfil" :alt="nome" class="circle responsive-img"> <!-- notice the "circle" class -->
+                    </grid-vue>
+                    <grid-vue tamanho="11">
+                        <span>
+                            <strong>{{nome}}</strong> - <small>{{data}}</small>
+                        </span>
+                    </grid-vue>
+                </div>
 
-            <slot>
-                
-            </slot>
-        </div>
-        <div class="card-action">
-            <p>
-                <i class='material-icons'>favorite_border</i>
-                <i class='material-icons'>insert_comment</i>
-            </p>
+                <slot>
+                    
+                </slot>
+            </div>
+            <div class="card-action">
+                <p>
+                    <i class='material-icons'>favorite_border</i>
+                    <i class='material-icons'>insert_comment</i>
+                </p>
+            </div>
         </div>
     </div>
 </template>
--- social/src/pages/home/Home.vue
+++ social/src/pages/home/Home.vue
@@ -1,22 +1,37 @@
 <template>
-    <card-conteudo-vue perfil='https://materializecss.com/images/yuna.jpg' nome='Rogerio Pereira' data='17/05/2019 12:30'>
-        <card-detalhe-vue 
-            imagem='https://materializecss.com/images/sample-1.jpg' 
-            titulo='Teste titulo'
-            texto='I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.'
-        />
-    </card-conteudo-vue>
+    <span>
+        <div class="row">
+            <grid-vue class="input-field" tamanho='12'>
+                <textarea id="textarea1" class="materialize-textarea"></textarea>
+                <label for="textarea1">O que está acontecendo?</label>
+            </grid-vue>
+            
+            <p>
+                <grid-vue class='btn waves-effect waves-light' tamanho='2 offset-s10'>Publicar</grid-vue>
+            </p>
+        </div>
+
+        <card-conteudo-vue perfil='https://materializecss.com/images/yuna.jpg' nome='Rogerio Pereira' data='17/05/2019 12:30'>
+            <card-detalhe-vue 
+                imagem='https://materializecss.com/images/sample-1.jpg' 
+                titulo='Teste titulo'
+                texto='I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.'
+            />
+        </card-conteudo-vue>
+    </span>
 </template>
 
 <script>
 import CardConteudoVue from '@/components/social/CardConteudoVue'
 import CardDetalheVue from '@/components/social/CardDetalheVue'
+import GridVue from '@/components/layouts/GridVue'
 
 export default {
     name: 'Home',
     components: {
         CardConteudoVue,
-        CardDetalheVue
+        CardDetalheVue,
+        GridVue
     },
     data () {
         return {
