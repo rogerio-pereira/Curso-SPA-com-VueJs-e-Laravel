1c8b661c78b618cffe8b4afb8044c813a653ae54
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-05-17 18:46:25 -0600

    Seção 1, Aula16 - Componente Detalhe Conteúdo

--- social/src/components/social/CardConteudoVue.vue
+++ social/src/components/social/CardConteudoVue.vue
@@ -13,15 +13,7 @@
             </div>
 
             <slot>
-                <div class="card-image">
-                    <img src="https://materializecss.com/images/sample-1.jpg">
-                    <span class="card-title">Card Title</span>
-                </div>
-                <div class="card-content">
-                    <p>
-                        I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.
-                    </p>
-                </div>
+                
             </slot>
         </div>
         <div class="card-action">
--- social/src/pages/home/Home.vue
+++ social/src/pages/home/Home.vue
@@ -1,15 +1,22 @@
 <template>
     <card-conteudo-vue perfil='https://materializecss.com/images/yuna.jpg' nome='Rogerio Pereira' data='17/05/2019 12:30'>
+        <card-detalhe-vue 
+            imagem='https://materializecss.com/images/sample-1.jpg' 
+            titulo='Teste titulo'
+            texto='I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.'
+        />
     </card-conteudo-vue>
 </template>
 
 <script>
 import CardConteudoVue from '@/components/social/CardConteudoVue'
+import CardDetalheVue from '@/components/social/CardDetalheVue'
 
 export default {
     name: 'Home',
     components: {
-        CardConteudoVue
+        CardConteudoVue,
+        CardDetalheVue
     },
     data () {
         return {
