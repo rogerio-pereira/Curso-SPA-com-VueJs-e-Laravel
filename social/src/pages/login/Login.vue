cb787993a9fefbbd16bc5bc63400410d8ff0084b
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-05-18 18:57:56 -0600

    Seção 1, Aula 23 - Formulario de Login e Cadastro

--- social/src/pages/login/Login.vue
+++ social/src/pages/login/Login.vue
@@ -4,31 +4,40 @@
             <img src='https://bizcapital.com.br/blog/wp-content/uploads/2019/02/Img_Redes_Sociais_para_Neg%C3%B3cios_2-2-848x400.png' class='responsive-img'>
         </span>
         <span slot='principal'>
-            <h2>Login</h2>
+            <span v-if='!cadastro'>
+                <h2>Login</h2>
 
-            <input type='email' placeholder='E-mail'>
-            <input type='password' placeholder='Senha'>
-            <button class='btn'>Entrar</button>
+                <input type='email' placeholder='E-mail'>
+                <input type='password' placeholder='Senha'>
+                <button class='btn'>Entrar</button>
+                <button class='btn orange' v-on:click='cadastro = !cadastro'>Cadastre-se</button>
+            </span>
+
+            <span v-if='cadastro'>
+                <h2>Cadastro</h2>
+
+                <input type='text' placeholder='Nome'>
+                <input type='email' placeholder='E-mail'>
+                <input type='password' placeholder='Senha'>
+                <input type='password' placeholder='Confirme a senha'>
+                <button class='btn'>Enviar</button>
+                <button class='btn orange' v-on:click='cadastro = !cadastro'>Ja tenho conta</button>
+            </span>
         </span>
     </login-template>
 </template>
 
 <script>
 import LoginTemplate from '@/templates/LoginTemplate'
-import CardConteudoVue from '@/components/social/CardConteudoVue'
-import CardDetalheVue from '@/components/social/CardDetalheVue'
-import PublicarConteudoVue from '@/components/social/PublicarConteudoVue'
 
 export default {
     name: 'Login',
     components: {
-        LoginTemplate,
-        CardConteudoVue,
-        CardDetalheVue,
-        PublicarConteudoVue
+        LoginTemplate
     },
     data () {
         return {
+            cadastro:false
         }
     }
 }
