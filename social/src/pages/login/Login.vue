d1f83aedc8532cf638e7532fe5cdbf9f737633f1
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-05-18 18:06:24 -0600

    Seção 1, Aula 20 - Organizando com Slots

--- social/src/pages/login/Login.vue
+++ social/src/pages/login/Login.vue
@@ -1,10 +1,15 @@
 <template>
     <login-template>
-        <h2>Login</h2>
+        <span slot='menu-esquerdo'>
+            <img src='https://bizcapital.com.br/blog/wp-content/uploads/2019/02/Img_Redes_Sociais_para_Neg%C3%B3cios_2-2-848x400.png' class='responsive-img'>
+        </span>
+        <span slot='principal'>
+            <h2>Login</h2>
 
-        <input type='email' placeholder='E-mail'>
-        <input type='password' placeholder='Senha'>
-        <button class='btn'>Entrar</button>
+            <input type='email' placeholder='E-mail'>
+            <input type='password' placeholder='Senha'>
+            <button class='btn'>Entrar</button>
+        </span>
     </login-template>
 </template>
 
