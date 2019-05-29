789a1c8b3e4464a7e9748cb3708612dd14574eb3
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-05-29 15:34:21 -0500

    Seção 1, Aula 55 - Modelo de como adicionar Conteudos

--- webservice/routes/api.php
+++ webservice/routes/api.php
@@ -14,3 +14,18 @@ Route::post('/cadastro', 'UsuarioController@cadastro');
 Route::post('/login', 'UsuarioController@login');
 Route::middleware('auth:api')->get('/usuario', 'UsuarioController@usuario');
 Route::middleware('auth:api')->put('/perfil', 'UsuarioController@perfil');
+
+Route::get('/testes', function(){
+    $user = App\User::find(1);
+    
+    //Cria Conteudo
+    /*$user->conteudos()->create([
+        'titulo' => 'Conteudo 3',
+        'texto' => 'Aqui o texto',
+        'imagem' => 'URL da imagem',
+        'link' => 'Link',
+        'data' => date('Y-m-d'),
+    ]);
+
+    return $user->conteudos;*/
+});
