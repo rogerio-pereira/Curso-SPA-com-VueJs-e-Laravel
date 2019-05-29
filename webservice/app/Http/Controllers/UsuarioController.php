96fff750bbcde238d7ea3a8d9c2d92dcdc9c1d3a
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-05-29 12:23:18 -0500

    Seção 1, Aula 50 - Passando a Logica para o Controller

--- webservice/app/Http/Controllers/UsuarioController.php
+++ webservice/app/Http/Controllers/UsuarioController.php
@@ -2,14 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
 use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class UsuarioController extends Controller
 {
+    public function cadastro(Request $request)
+    {
+        $data = $request->all();
+
+        $validacao = Validator::make($data, [
+                        'name' => 'required|string|max:255',
+                        'email' => 'required|string|email|max:255|unique:users',
+                        'password' => 'required|string|min:6|confirmed',
+                    ]);
+
+        if($validacao->fails())
+            return $validacao->errors();
+
+        $imagem = '/perfils/padrao.png';
+        $data['imagem'] = $imagem;
+
+        $data['password'] = bcrypt($data['password']);
+        $user = User::create($data);
+        $user->token = $user->createToken($user->email)->accessToken;
+
+        $user->imagem = asset($user->imagem); 
+        return $user;
+    }
+
     public function login(Request $request)
     {
         $data = $request->all();
@@ -31,5 +56,109 @@ class UsuarioController extends Controller
         }
         else
             return ['status' => false];
+    }
+
+    public function perfil(Request $request)
+    {
+        $user = $request->user();
+        $data = $request->all();
+
+        //Validação com Senha
+        if(isset($data['password'])) {
+            $validacao = Validator::make($data, [
+                            'name' => 'required|string|max:255',
+                            'email' => ['required','string','email','max:255',Rule::unique('users')->ignore($user->id)],
+                            'password' => 'required|string|min:6|confirmed',
+                        ]);
+
+            $user->password = bcrypt($data['password']);
+        }
+        //Validação sem senha
+        else {
+            $validacao = Validator::make($data, [
+                            'name' => 'required|string|max:255',
+                            'email' => ['required','string','email','max:255',Rule::unique('users')->ignore($user->id)],
+                        ]);
+        }
+
+        if($validacao->fails())
+            return $validacao->errors();
+
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+
+        //Imagem
+        if(isset($data['imagem'])) {
+            Validator::extend('base64image', function ($attribute, $value, $parameters, $validator){
+                $explode = explode(',', $value);
+                $allow = ['png', 'jpg', 'jpeg', 'svg'];
+                $format = str_replace([
+                                        'data:image/', 
+                                        ';',
+                                        'base64',
+                                    ],
+                                    [
+                                        '', '', '',
+                                    ],
+                                    $explode[0]
+                                );
+
+                //Check file format
+                if(!in_array($format, $allow)) {
+                    return false;
+                }
+
+                //Check base64 format
+                if(!preg_match('%^[a-zA-Z0-9/+]*={0,2}$%', $explode[1])) {
+                    return false;
+                }
+
+                return true;
+            });
+
+
+            $validacao = Validator::make($data, [
+                                'imagem' => 'base64image',
+                            ],
+                            [
+                                'base64image' => 'Imagem inválida'
+                            ]);
+
+            if($validacao->fails())
+                return $validacao->errors();
+
+            $time = time();
+            $diretorioPai = 'perfils';
+            $diretorioImagem = $diretorioPai.DIRECTORY_SEPARATOR.'perfil_id'.$user->id;
+
+            //Pegar extensão da imagem
+            $ext = substr($data['imagem'], 11, strpos($data['imagem'], ';') -11);
+            $urlImagem = $diretorioImagem.DIRECTORY_SEPARATOR.$time.'.'.$ext;
+
+            $file = str_replace('data:image/'.$ext.';base64,', '', $data['imagem']);
+            $file = base64_decode($file);
+
+            if($user->imagem){
+                if(file_exists($user->imagem)) {
+                    Storage::delete($user->imagem);
+                }
+            }
+
+            Storage::put($urlImagem, $file);
+
+            $user->imagem = $urlImagem; 
+        }
+
+        $user->save();
+
+        $user->imagem = asset($user->imagem); 
+        $user->token = $user->createToken($user->email)->accessToken;
+
+        return $user;
+    }
+
+    public function usuario(Request $request)
+    {
+        return $request->user();
     }
 }
