d771f4b8c26d409d306ef75ca5121373d4cd270a
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2019-05-31 10:24:49 -0500

    Seção 1, Aula 84 - Criando a Rota de Pagina do Usuario

---
+++ social/src/pages/pagina/Pagina.vue
@@ -0,0 +1,145 @@
+<template>
+    <site-template>
+        <span slot='menu-esquerdo'>
+            <card-menu-vue>
+                <div class="row valign-wrapper">
+                    <grid-vue tamanho="4">
+                        <img :src="usuario.imagem" :alt="usuario.name" class="circle responsive-img"> <!-- notice the "circle" class -->
+                    </grid-vue>
+                    <grid-vue tamanho="8">
+                        <span class="black-text">
+                            <h5>{{usuario.name}}</h5>
+                        </span>
+                    </grid-vue>
+                </div>
+            </card-menu-vue>
+            <card-menu-vue>
+                <h3>Amigos</h3>
+                <li>Murilo</li>
+                <li>Gustavo</li>
+            </card-menu-vue>
+        </span>
+
+        <span slot='principal'>
+            <publicar-conteudo-vue />
+
+            <card-conteudo-vue 
+                v-for='item in listaConteudos' 
+                :key='item.id' 
+                :id='item.id' 
+                :comentarios='item.comentarios'
+                :totalcurtidas='item.curtidas'
+                :curtiuconteudo='item.curtiuconteudo'
+                :perfil='item.user.imagem' 
+                :nome='item.user.name' 
+                :data='item.data'
+            >
+                <card-detalhe-vue 
+                    :imagem='item.imagem' 
+                    :titulo='item.titulo'
+                    :texto='item.texto'
+                    :link='item.link'
+                />
+            </card-conteudo-vue>
+
+            <div v-scroll='handleScroll'></div>
+        </span>
+    </site-template>
+</template>
+
+<script>
+import SiteTemplate from '@/templates/SiteTemplate'
+import CardConteudoVue from '@/components/social/CardConteudoVue'
+import CardDetalheVue from '@/components/social/CardDetalheVue'
+import PublicarConteudoVue from '@/components/social/PublicarConteudoVue'
+import CardMenuVue from '@/components/layouts/CardMenuVue'
+import GridVue from '@/components/layouts/GridVue'
+
+export default {
+    name: 'Pagina',
+    components: {
+        SiteTemplate,
+        CardConteudoVue,
+        CardDetalheVue,
+        PublicarConteudoVue,
+        CardMenuVue,
+        GridVue
+    },
+    data () {
+        return {
+            usuario:false,
+            urlProximaPagina: null,
+            pararScroll:false
+        }
+    },
+    created() {
+        let usuarioAux = this.$store.getters.getUsuario;
+
+        if(usuarioAux) {
+            this.usuario = this.$store.getters.getUsuario;
+
+            this.$http.get(this.$urlAPI+'conteudo/lista', {
+                'headers':{
+                    'authorization': 'Bearer '+this.$store.getters.getToken
+                }
+            })
+            .then(response => {
+                //console.log(response)
+                if(response.data.status) {
+                    this.$store.commit('setConteudosLinhaTempo', response.data.conteudos.data);
+                    this.urlProximaPagina = response.data.conteudos.next_page_url;
+                }
+            })
+            .catch(e => {
+                console.log(e)
+                alert('Erro! Tente novamente mais tarde')
+            })
+        }
+    },
+    computed:{
+        listaConteudos(){
+            return this.$store.getters.getConteudosLinhaTempo;
+        }
+    },
+    methods: {
+        handleScroll() {
+            // console.log(window.scrollY);
+            // console.log(document.body.clientHeight);
+            if(this.pararScroll)
+                return;
+
+            if(window.scrollY >= (document.body.clientHeight - 1069)) {
+                this.pararScroll = true;
+                this.carregaPaginacao();
+            }
+        },
+        carregaPaginacao() {
+            if(!this.urlProximaPagina) 
+                return;
+
+            this.$http.get(this.urlProximaPagina, {
+                'headers':{
+                    'authorization': 'Bearer '+this.$store.getters.getToken
+                }
+            })
+            .then(response => {
+                //console.log(response)
+                if(response.data.status) {
+                    this.$store.commit('setPaginacaoConteudosLinhaTempo', response.data.conteudos.data);
+                    this.urlProximaPagina = response.data.conteudos.next_page_url;
+                    this.pararScroll = false;
+                }
+            })
+            .catch(e => {
+                console.log(e)
+                alert('Erro! Tente novamente mais tarde')
+            })
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
